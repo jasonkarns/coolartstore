182b326e55dc99c26e86fda6159ac870b17efb5e
Jason Karns <jason@karns.name>
2013-06-09 17:05:53 -0400

    fix JSHint errors
    function called before defined
    radix should be used with parseInt

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,6 @@
 
-function init() {
-  $.get( "results", function( info ) {
-    $( "div.results" ).html( JST['app/templates/results.handlebars']( info ) );
-  });
-  $( "div.content" ).on( "click", "div.result", showDetail );
-}
-
-function showDetail( e ) {
-  var id = $( this ).data( "id" );
-  loadDetail( id );
-  $( "div.detail" ).show();
+function loadBio( artistId ) {
+  // some stuff here, definitely needs a template
 }
 
 function loadDetail( id ) {
@@ -18,14 +9,23 @@ function loadDetail( id ) {
   }
   $.get( "detail/" + id, function( info ) {
     $( "div.detail" ).html( JST['app/templates/detail.handlebars']( info ) );
-    $( "div.detail a.prev" ).on( "click", { id: parseInt( id ) - 1 }, loadDetail );
-    $( "div.detail a.next" ).on( "click", { id: parseInt( id ) + 1 }, loadDetail );
+    $( "div.detail a.prev" ).on( "click", { id: parseInt( id, 10 ) - 1 }, loadDetail );
+    $( "div.detail a.next" ).on( "click", { id: parseInt( id, 10 ) + 1 }, loadDetail );
     $( "div.detail div.artist a" ).on( "click", loadBio );
   });
 }
 
-function loadBio( artistId ) {
-  // some stuff here, definitely needs a template
+function showDetail( e ) {
+  var id = $( this ).data( "id" );
+  loadDetail( id );
+  $( "div.detail" ).show();
+}
+
+function init() {
+  $.get( "results", function( info ) {
+    $( "div.results" ).html( JST['app/templates/results.handlebars']( info ) );
+  });
+  $( "div.content" ).on( "click", "div.result", showDetail );
 }
 
 init();
